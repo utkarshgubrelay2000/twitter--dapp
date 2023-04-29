ddcf5c09786997e2826335d275dcc54390162e82
utkarsh-tbw <utkarsh@thebrandwick.com>
2023-04-29 22:19:16 +0530

    res

--- client/context/ContractLoader.js
+++ client/context/ContractLoader.js
@@ -4,6 +4,7 @@ export const LoadContract=async (name)=>{
    
         let res=await axios.get(`/contracts/${name}.sol/${name}.json`)
         res=res.data
+        console.log(res)
         let abi=res.abi
         return abi;
       
