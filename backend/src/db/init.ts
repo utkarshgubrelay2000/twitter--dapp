34aa8040a0bfcabbec6071be52eb1c3b73dae1e4
utkarsh-tbw <utkarsh@thebrandwick.com>
2023-05-20 15:59:05 +0530

    network sepolia

--- backend/src/db/init.ts
+++
@@ -1,9 +0,0 @@
-
-import {  user } from "./models"
-
-const isDev = process.env.NODE_ENV === 'development'
-
-const dbInit = () => {
-  user.sync({alter:true})
-}
-export default dbInit 
--- backend/src/helpers/utils.js
+++
@@ -1,9 +0,0 @@
-import {ethers,JsonRpcApiProvider} from 'ethers' 
-var url = 'https://goerli.infura.io/v3/b17f99e3549d496da4bf879ecde791e8';
-export const eth=async()=>{
-    let customHttpProvider = new ethers.InfuraProvider('goerli')
-  let a=await customHttpProvider.getBalance('0xac524DdD17f88e7b0F47ad20517B54cEFbc0a0f2')
-  console.log(ethers.toBigInt(a))
-}
-
-export default eth;
